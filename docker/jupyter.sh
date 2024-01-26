7290fe17832372e6052a975233be689340302a84
James W. McClain <jmcclain@daystrom-data-concepts.com>
2024-01-26 11:45:42 -0500

    Add Llama 2 support

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-docker run -p 8888:8888 --env PYTHONPATH=/workdir --shm-size ${2:-16}G --runtime=nvidia -it --rm -v $(pwd):/workdir -v ${1:-/tmp}:/datasets:rw -w /workdir llm-finetune
+docker run -p 8888:8888 --env PYTHONPATH=/workdir --shm-size ${2:-16}G --runtime=nvidia -it --rm -v $(pwd):/workdir -v ${1:-/tmp}:/models:ro -w /workdir llm-finetune
